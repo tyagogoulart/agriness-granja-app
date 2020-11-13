6a2a85881a03f136b0dc6c4834807980815d67ad
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-13 06:10:34 -0300

    add container to list granjas

--- src/containers/granjas-list.tsx
+++ src/containers/granjas-list.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import GranjaCard from '../components/granja-card';
 import { RootState } from '../store/reducers';
+import { listUserGranjasRequest } from '../store/reducers/user/actions';
 
 const GranjasList: React.FC = () => {
   const granjas = useSelector((state: RootState) => state.user.granjas);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(listUserGranjasRequest());
+  }, []);
 
   return (
     <View style={styles.list}>
@@ -20,7 +26,6 @@ const GranjasList: React.FC = () => {
 const styles = StyleSheet.create({
   list: {
     flex: 1,
-    margin: 16,
   },
 });
 
