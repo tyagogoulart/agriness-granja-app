cf6ed060a7ee123beaf88420b3d05dc7b9a643b5
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-13 05:57:37 -0300

    add screens navigations types

--- src/navigator.ts
+++ src/navigator.ts
@@ -3,11 +3,26 @@ import { createStackNavigator, StackNavigationProp } from '@react-navigation/sta
 
 export type RootStackParamList = {
   Home: undefined;
-  AnimalDetails: undefined;
+  AnimalDetail: undefined;
+  Login: undefined;
+  GranjaAnimals: undefined;
+  AnimalUpdate: { animalId: string };
 };
 
 export type HomeScreenRouteProp = RouteProp<RootStackParamList, 'Home'>;
 
 export type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+
+export type AnimalDetailScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'AnimalDetail'
+>;
+
+export type GranjaAnimalsScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'GranjaAnimals'
+>;
+
+export type GranjaAnimalsScreenRouteProp = RouteProp<RootStackParamList, 'GranjaAnimals'>;
 
 export const Stack = createStackNavigator<RootStackParamList>();
