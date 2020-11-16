9668529fa47382646c7040e02debb6b49a77a224
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-16 02:06:47 -0300

    refactor screen to use components

--- src/screens/granja-animals-screen.tsx
+++ src/screens/granja-animals-screen.tsx
@@ -1,46 +1,14 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
-import React, { ReactText, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 
-import AnimalsFilters from '../components/animals-filters';
-import Container from '../components/container';
-import ScreenTitle from '../components/screen-title';
-import AnimalsList from '../containers/animals-list';
-import { GranjaAnimalsScreenNavigationProp } from '../navigator';
-import { RootState } from '../store/reducers';
+import GranjaAnimalsContent from '../containers/granja-animals-content';
+import { GranjaAnimalsScreenRouteProp } from '../navigator';
 
-type RouteParams = {
-  GranjaAnimals: {
-    granjaId: number;
-  };
+type Props = {
+  route: GranjaAnimalsScreenRouteProp;
 };
 
-type FilterProps = {
-  name: string;
-  location: ReactText;
-};
-
-type Props = { navigation: GranjaAnimalsScreenNavigationProp };
-
-type RouteProps = RouteProp<RouteParams, 'GranjaAnimals'>;
-
-const GranjaAnimalsScreen: React.FC<Props> = ({ navigation }) => {
-  const { params } = useRoute<RouteProps>();
-  const [granja] = useSelector((state: RootState) =>
-    state.user.granjas.filter((granja) => granja.id === params.granjaId)
-  );
-  const [filters, setFilters] = useState<FilterProps>({
-    name: '',
-    location: '',
-  });
-
-  return (
-    <Container>
-      <ScreenTitle title={granja.nome} />
-      <AnimalsFilters filters={filters} setFilters={setFilters} granjaId={params.granjaId} />
-      <AnimalsList filters={filters} granjaId={params.granjaId} />
-    </Container>
-  );
+const GranjaAnimalsScreen: React.FC<Props> = ({ route }) => {
+  return <GranjaAnimalsContent granjaId={route.params.granjaId} />;
 };
 
 export default GranjaAnimalsScreen;
