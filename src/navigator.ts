f40f47a67629c46f8befe9167b777a989e9756b9
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-16 01:55:09 -0300

    define navigators params and types

--- src/navigator.ts
+++ src/navigator.ts
@@ -3,26 +3,25 @@ import { createStackNavigator, StackNavigationProp } from '@react-navigation/sta
 
 export type RootStackParamList = {
   Home: undefined;
-  AnimalDetail: undefined;
+  AnimalDetail: { animalId: string };
   Login: undefined;
-  GranjaAnimals: undefined;
+  GranjaAnimals: { granjaId: number };
   AnimalUpdate: { animalId: string };
 };
 
 export type HomeScreenRouteProp = RouteProp<RootStackParamList, 'Home'>;
-
 export type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
+export type AnimalDetailScreenRouteProp = RouteProp<RootStackParamList, 'AnimalDetail'>;
 export type AnimalDetailScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'AnimalDetail'
 >;
 
+export type GranjaAnimalsScreenRouteProp = RouteProp<RootStackParamList, 'GranjaAnimals'>;
 export type GranjaAnimalsScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'GranjaAnimals'
 >;
-
-export type GranjaAnimalsScreenRouteProp = RouteProp<RootStackParamList, 'GranjaAnimals'>;
 
 export const Stack = createStackNavigator<RootStackParamList>();
