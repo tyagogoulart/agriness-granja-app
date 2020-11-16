14102ea45325ad6cbc8a96707b5bc9481603f64e
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-16 02:01:01 -0300

    add component to show header in animal detail

---
+++ src/containers/animal-detail-header.tsx
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import ScreenTitle from '../components/screen-title';
+import { RootState } from '../store/reducers';
+import { statusIcon } from '../utils/animal-status-helpers';
+
+type Props = {
+  animalId: string;
+};
+
+const AnimalDetailHeader: React.FC<Props> = ({ animalId }) => {
+  const [animal] = useSelector((state: RootState) =>
+    state.granja.animals.filter((animal) => animal.id === animalId)
+  );
+
+  return (
+    <View>
+      <ScreenTitle title={animal.nome}>
+        <Text style={{ marginTop: 5, marginLeft: 8 }}>
+          <FontAwesomeIcon icon={statusIcon(animal.status_animal)} />
+        </Text>
+      </ScreenTitle>
+      <Text style={styles.code}>{animal.codigo_rastreamento}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  code: {
+    fontSize: 10,
+    color: '#315ea8',
+    marginBottom: 16,
+  },
+});
+
+export default AnimalDetailHeader;
