dd5f41b2f520c1753a83c5a3582eec949d7cf86c
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-16 02:03:37 -0300

    add field validations and error messages

--- src/containers/login-form.tsx
+++ src/containers/login-form.tsx
@@ -1,37 +1,69 @@
-import React, { useState } from 'react';
-import { Button, Image, StyleSheet, View } from 'react-native';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Input from '../components/input';
-import { LogInRequest } from '../store/reducers/auth/actions';
+import { RootState } from '../store/reducers';
+import {
+  LogInRequest,
+  SetLoginCredentialRequest,
+  validateEmailRequest,
+  validatePasswordMinLengthRequest,
+  validateRequiredFieldRequest,
+} from '../store/reducers/auth/actions';
 
 const LoginForm: React.FC = () => {
+  const { error, isLoading, credentials } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+
+  useEffect(() => {
+    const { password } = credentials;
+    if (password.length >= 1 && password.length <= 4)
+      dispatch(validatePasswordMinLengthRequest(password, 4));
+  }, [credentials.password]);
+
+  useEffect(() => {
+    const { email } = credentials;
+    if (email.length >= 1) dispatch(validateEmailRequest(email));
+  }, [credentials.email]);
 
   const onPress = () => {
-    dispatch(LogInRequest({ email, password }));
+    const { email, password } = credentials;
+    if (!email) dispatch(validateRequiredFieldRequest('email', email));
+    if (!password) dispatch(validateRequiredFieldRequest('password', password));
+    if (email && password) {
+      dispatch(LogInRequest(credentials));
+    }
   };
 
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require('../assets/img/agriness.png')} />
-      <Input
-        style={styles.input}
-        keyboardType="email-address"
-        value={email}
-        placeholder="Seu e-mail"
-        onChangeText={(value) => setEmail(value)}
-      />
-      <Input
-        style={styles.input}
-        secureTextEntry
-        value={password}
-        placeholder="Sua senha"
-        onChangeText={(value) => setPassword(value)}
-      />
-      <Button color="#315ea8" title="Entrar" onPress={onPress} />
+      <View style={{ width: '100%' }}>
+        <Text style={styles.error}>{error?.detail}</Text>
+        <Input
+          style={[styles.input]}
+          keyboardType="email-address"
+          value={credentials.email}
+          placeholder="Seu e-mail"
+          onChangeText={(value) => dispatch(SetLoginCredentialRequest('email', value))}
+          error={error?.email}
+        />
+        <Input
+          style={[styles.input, { marginBottom: 8 }]}
+          secureTextEntry
+          value={credentials.password}
+          placeholder="Sua senha"
+          onChangeText={(value) => dispatch(SetLoginCredentialRequest('password', value))}
+          error={error?.password}
+        />
+        <Button
+          disabled={isLoading || !!error?.email || !!error?.password || !!error?.detail}
+          color="#315ea8"
+          title="Entrar"
+          onPress={onPress}
+        />
+      </View>
     </View>
   );
 };
@@ -50,10 +82,10 @@ const styles = StyleSheet.create({
     marginBottom: 32,
     marginTop: 16,
   },
-  image: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+  error: {
+    textAlign: 'center',
+    marginBottom: 16,
+    color: 'red',
   },
 });
 
