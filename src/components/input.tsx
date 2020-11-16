b65cafeaddca35b4e703aa1f30781d9857e21a64
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-16 02:00:09 -0300

    add errors state and colors to input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import { StyleSheet, TextInputProps } from 'react-native';
-import { TextInput, NativeViewGestureHandlerProperties } from 'react-native-gesture-handler';
+import { StyleSheet, TextInputProps, View, TextInput, StyleProp, ViewStyle } from 'react-native';
 
-type Props = NativeViewGestureHandlerProperties & TextInputProps;
+import ErrorMessage from './error-message';
+
+type Props = TextInputProps & { error?: string; wrapperStyle?: StyleProp<ViewStyle> };
 
 const Input: React.FC<Props> = (props) => {
-  return <TextInput {...props} style={[styles.input, props.style]} />;
+  const hasError = props.error !== undefined && props.error !== '';
+
+  return (
+    <View style={[props.wrapperStyle, styles.wrapper]}>
+      <TextInput
+        {...props}
+        style={[styles.input, props.style, { borderColor: hasError ? 'red' : '#cecece' }]}
+      />
+      {hasError && <ErrorMessage message={props.error} style={styles.error} />}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    marginBottom: 16,
+  },
   input: {
     backgroundColor: '#ffffff',
     borderWidth: 1,
     borderColor: '#cecece',
-    marginBottom: 16,
     borderRadius: 4,
     fontSize: 15,
     color: '#000000',
     height: 40,
     padding: 8,
+  },
+  error: {
+    marginTop: 4,
+    textAlign: 'left',
   },
 });
 
