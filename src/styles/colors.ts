d5dd1d1328f29261c094b579c7611ae6a010fbff
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-16 21:27:27 -0300

    add global styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+type ColorsTypes = {
+  primary: string;
+  dark: string;
+  error: string;
+  lightGrey: string;
+  background: string;
+};
+
+const colors: ColorsTypes = {
+  primary: '#315ea8',
+  dark: '#000000',
+  error: '#b3190e',
+  lightGrey: '#cecece',
+  background: '#fafafa',
+};
+
+export default colors;
---
+++ src/styles/metrics.ts
@@ -0,0 +1,15 @@
+type MetricsTypes = {
+  smallMargin: number;
+  baseMargin: number;
+  doubleBaseMargin: number;
+  baseRadius: number;
+};
+
+const metrics: MetricsTypes = {
+  smallMargin: 8,
+  baseMargin: 16,
+  doubleBaseMargin: 32,
+  baseRadius: 4,
+};
+
+export default metrics;
